974d61a726716a2c58fc70aac6639d0c5b1c64c8
Ibrahim <sk.mohammed5811@gmail.com>
2025-11-08 22:50:53 +0530

    Initial push

---
+++ Spring-AI-Project/src/test/java/com/spring/ai/Spring/AI/Project/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.Spring.AI.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
