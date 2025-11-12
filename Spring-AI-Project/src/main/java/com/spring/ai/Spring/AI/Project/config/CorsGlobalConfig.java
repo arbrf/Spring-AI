c27a2c3de644f2518077ae0c77a507f3f9357edc
Ibrahim <sk.mohammed5811@gmail.com>
2025-11-12 14:24:48 +0530

    Open-ai configurations completed

---
+++ Spring-AI-Project/src/main/java/com/spring/ai/Spring/AI/Project/config/CorsGlobalConfig.java
@@ -0,0 +1,24 @@
+package com.spring.ai.Spring.AI.Project.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+        System.out.println("✅ CORS configured for MVC (Tomcat)");
+    }
+}
